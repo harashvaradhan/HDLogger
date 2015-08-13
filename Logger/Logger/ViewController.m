9dcb2059a0e8d95288716b3bb1f190cf1a37863d
GNR solution PVT.LTD <GNR@GNRs-MacBook-Pro.local>
2015-08-13 13:52:30 +0530

    Log file email

--- Logger/Logger/ViewController.m
+++ Logger/Logger/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 #import "HDLogger.h"
 
-@interface ViewController ()
+
+#import <MessageUI/MessageUI.h>
+
+
+@interface ViewController ()<MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -27,6 +31,12 @@
     NSLog(@"Printing file contents");
     NSLog(@"\n%@",[[HDLogger sharedInstance]getLogFilesContentWithMaxSize:1000]);
     
+    
+    UIButton *btnComposeEmail = [[UIButton alloc]initWithFrame:CGRectMake(10, 300, 300, 30)];
+    [btnComposeEmail setTitle:@"Email Log" forState:UIControlStateNormal];
+    btnComposeEmail.backgroundColor = [UIColor redColor];
+    [btnComposeEmail addTarget:self action:@selector(openMailComposer:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btnComposeEmail];
     /*
     NSDictionary *d = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"]];
     NSString *s = [d objectForKey:@"CFBundleExecutable"];
@@ -34,6 +44,62 @@
     
     NSLog(@"Pr %@",[[[NSBundle mainBundle] infoDictionary]objectForKey:@"CFBundleName"]);*/
 }
+
+#pragma mark -
+#pragma mark -
+#pragma mark -
+
+-(void)openMailComposer:(UIButton *)sender{
+    
+    NSString *emailTitle = @"Feedback";
+    // Email Content
+    NSString *messageBody = @"Please type your feedback here:";
+    // To address
+    NSArray *toRecipents = [NSArray arrayWithObject:@"jude@packedful.com"];
+    
+    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+    mc.mailComposeDelegate = self;
+    [mc setSubject:emailTitle];
+    [mc setMessageBody:messageBody isHTML:NO];
+    [mc setToRecipients:toRecipents];
+    
+    NSMutableData *data = [NSMutableData dataWithContentsOfFile:[[HDLogger sharedInstance] documentDirectoryLogFileLocation]];
+    [mc addAttachmentData:data mimeType:@"application/log" fileName:[NSString stringWithFormat:@"%@.log",[[[NSBundle mainBundle] infoDictionary]objectForKey:@"CFBundleName"]]];
+    
+    [self presentViewController:mc animated:YES completion:NULL];
+
+}
+
+#pragma mark -
+#pragma mark - Mail Composer Delegate
+#pragma mark -
+
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    
+    // Close the Mail Interface
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    //   [self.navigationController presentViewController:HomeVC animated:YES completion:nil];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
